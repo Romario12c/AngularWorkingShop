4cc39e7eb15957f6c8311f96c0c12701e40c1a72
Romario12c <romario.chung@outlook.com>
2020-10-09 01:23:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7qemws-6qbhnd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7qemws-6qbhnd)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'BigBooty Latinas V',
+    price: 799,
+    description: 'A large booty latina running the train'
+  },
+  {
+    name: 'Hose',
+    price: 699,
+    description: 'A great tight hose'
+  },
+  {
+    name: 'Elephant',
+    price: 299,
+    description: "it's an elephant "
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
